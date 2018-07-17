15b839214debb1417e367c0954d2a4fdca3e3f13
Daniil Sharko <integrex@yandex.ru>
2018-07-17 16:05:18 +0500

    change compiled regex to string

--- message_parser.py
+++ message_parser.py
@@ -1,14 +1,14 @@
 import re
 
-COMMON_TRIGGER_PATTERN = re.compile('.*го.+?есть.+?в\s+(.*)', re.IGNORECASE)
-DINNER_TIME_TRIGGER_PATTERN = re.compile('\s*когда\s*(?:на)?\s*обед\??\s*', re.IGNORECASE)
-DINNER_PLACE_TRIGGER_PATTERN = re.compile('\s*куда\s*(?:на)?\s*обед\??\s*', re.IGNORECASE)
-BREAKFAST_TIME_TRIGGER_PATTERN = re.compile('\s*куда\s*(?:на)?\s*завтрак\??\s*', re.IGNORECASE)
+COMMON_TRIGGER_PATTERN = '.*го.+?есть.+?в\s+(.*)'
+DINNER_TIME_TRIGGER_PATTERN = '\s*когда\s*(?:на)?\s*обед\??\s*'
+DINNER_PLACE_TRIGGER_PATTERN = '\s*куда\s*(?:на)?\s*обед\??\s*'
+BREAKFAST_TIME_TRIGGER_PATTERN = '\s*когда\s*(?:на)?\s*завтрак\??\s*'
 
 SEP_PATTERN = re.compile('(?:[,;?!\s]+)(?:или)?', re.IGNORECASE)
 
 
 def get_suggestions_in_common_case(message_text):
-    raw_suggestion = COMMON_TRIGGER_PATTERN.findall(message_text)
+    raw_suggestion = re.findall(COMMON_TRIGGER_PATTERN, message_text)
     if raw_suggestion:
         return [op.strip() for op in re.split(SEP_PATTERN, raw_suggestion[0]) if op.strip()]
